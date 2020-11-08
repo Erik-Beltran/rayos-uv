3424eb027552b35575984b06f4f95e5b3464ea32
Erik-Beltran <erikb0696@gmail.com>
2020-11-08 18:40:40 -0500

    se agrega el diseño del footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./footer.css";
+import logo from '../Recomendaciones/img/sita-footer.png'
 
 const Footer = () => {
   return (
-    <div className="container container-footer">
+    <div className="contenedor-footer">
       <h3>Politécnico Colombiano Jaime Isaza Cadavid © 2020</h3>
 
       <p>
@@ -13,6 +14,7 @@ const Footer = () => {
         319 79 00 | NIT: 890980136-6
       </p>
       <p>
+        <img src={logo} alt="sita" className="logo-sita"></img>
         <b>Semillero de Investigacion de Telecomunicaciones Aplicadas</b>
         <br />
         Carrera 48 No. 7 – 151 | El Poblado | PBX: (+57 4) 444 76 54 - (+57 4)
