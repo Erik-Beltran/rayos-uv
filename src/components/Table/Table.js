b2ae7ee42ff5bd9516d1ab21ddc9bfed490eeef0
Andres Rivillas <trand0161@gmail.com>
2020-11-25 14:34:12 -0500

    removing comments and final stuff

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,32 +1,18 @@
 import React from "react";
-import {
-  BootstrapTable,
-  TableHeaderColumn,
-  // ButtonGroup,
-} from "react-bootstrap-table";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 
 function Table({ array }) {
-  // const createCustomButtonGroup = (props) => {
-  //   return (
-  //     <ButtonGroup className="my-custom-class" sizeClass="btn-group-md">
-  //       {props.exportCSVBtn}
-  //     </ButtonGroup>
-  //   );
-  // };
   const options = {
-    sizePerPage: 5, // which size per page you want to locate as default
-    pageStartIndex: 0, // where to start counting the pages
-    paginationSize: 3, // the pagination bar size.
-    // prePage: "Anterior", // Previous page button text
-    // nextPage: "Siguente", // Next page button text
-    firstPage: "First", // First page button text
-    lastPage: "Last", // Last page button text
-    paginationShowsTotal: false, // Accept bool or function
-    paginationPosition: "bottom", // default is bottom, top and both is all available
-    hideSizePerPage: true, // > You can hide the dropdown for sizePerPage
-    alwaysShowAllBtns: true, // Always show next and previous button
-    withFirstAndLast: false, // Hide the going to First and Last page button
-    // btnGroup: createCustomButtonGroup,
+    sizePerPage: 5,
+    pageStartIndex: 0,
+    paginationSize: 3,
+    firstPage: "First",
+    lastPage: "Last",
+    paginationShowsTotal: false,
+    paginationPosition: "bottom",
+    hideSizePerPage: true,
+    alwaysShowAllBtns: true,
+    withFirstAndLast: false,
   };
   return (
     <BootstrapTable
@@ -36,7 +22,6 @@ function Table({ array }) {
       className="table"
       striped
       hover
-      // exportCSV
       pagination
     >
       <TableHeaderColumn isKey dataField="id" hidden>
